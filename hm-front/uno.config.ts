25d8c893832d0c68e3d33143d17daf98f309c036
Yahir <yahir.eduardo.bravo@gmail.com>
2026-02-17 22:32:22 -0500

    feat: add some adjusts to vuejs

---
+++ hm-front/uno.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, presetIcons, presetWebFonts, presetWind4, transformerDirectives, transformerVariantGroup } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetWind4(),
+    presetIcons(),
+    presetWebFonts({
+      provider: 'fontsource',
+    }),
+  ],
+  transformers: [
+    transformerVariantGroup(),
+    transformerDirectives()
+  ],
+})
--- hm-front/vite.config.ts
+++ hm-front/vite.config.ts
@@ -3,10 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    UnoCSS(),
     vue(),
     vueDevTools(),
   ],
